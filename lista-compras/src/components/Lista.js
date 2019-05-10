22f7274390c34ba518fdb3a2170d8a02e4931b48
Sidney Sousa <sidney.sousa@ifms.edu.br>
2019-05-09 22:21:36 -0400

    incrementando o projeto lista-compras

--- lista-compras/src/components/Lista.js
+++ lista-compras/src/components/Lista.js
@@ -1,11 +1,82 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
+import { URL_API } from '../config';
 import adicionar from '../img/adicionar.png';
 import remover from '../img/remover.png';
 
 class Lista extends Component {
 
+    constructor() {
+        super();
+
+        this.state = {
+            item: {
+                produtoId: '',
+                produto: '',
+                quantidade: 1,
+                unidadeMedida: ''
+            },
+            listaDeCompras: [],
+            produtos: []
+        };
+
+        this.aoAlterarValor =
+            this.aoAlterarValor.bind(this);
+        this.carregarProdutos =
+            this.carregarProdutos.bind(this);
+        this.salvar = this.salvar.bind(this);
+        this.remover = this.remover.bind(this);
+    }
+
+    componentDidMount() {
+        this.carregarProdutos();
+    }
+
+    aoAlterarValor(evento) {
+        const nome = evento.target.name;
+        const valor = evento.target.value;
+        let item = { ...this.state.item };
+        if (nome === 'produto') {
+            const produtoSelecionado =
+                this.state.produtos.find(produto => produto._id === valor);
+            item['produto'] = produtoSelecionado.descricao;
+            item['unidadeMedida'] = produtoSelecionado.unidadeMedida;
+            item['produtoId'] = valor;
+        } else {
+            item['quantidade'] = valor;
+        }
+
+        this.setState({ item });
+    }
+
+    carregarProdutos() {
+        axios
+            .get(URL_API)
+            .then(
+                resposta =>
+                    this.setState({ produtos: resposta.data })
+            )
+            .catch(erro => console.log(erro));
+    }
+
+    salvar() { }
+    remover() { }
+
     render() {
+        const opcoesProdutos =
+            this.state.produtos.map(produto => {
+                return (
+                    <option
+                        value={produto._id}
+                        key={produto._id}>
+                        {produto.descricao}
+                    </option>
+                );
+            });
+
+
+
         return (
             <div>
 
@@ -14,19 +85,20 @@ class Lista extends Component {
                     <form>
 
                         <select
+                            value={this.state.item.produtoId}
+                            onChange={this.aoAlterarValor}
                             name="produto">
-                            <option>-- Escolha um produto --</option>
-                            <option>Carne bovina</option>
-                            <option>Creme dental</option>
-                            <option>Farinha de trigo</option>
+                            {opcoesProdutos}
                         </select>
 
                         <input
+                            value={this.state.item.quantidade}
+                            onChange={this.aoAlterarValor}
                             type="number"
                             placeholder="Quantidade"
                             name="quantidade"
                             min="1" />
-                        <span>Kg</span>
+                        <span>{this.state.item.unidadeMedida}</span>
 
                         <button>
                             <img src={adicionar} alt="Adicionar produto" />
@@ -45,33 +117,6 @@ class Lista extends Component {
                             </tr>
                         </thead>
                         <tbody>
-                            <tr>
-                                <td>Farinha de trigo</td>
-                                <td>1 kg</td>
-                                <td>
-                                    <button>
-                                        <img src={remover} alt="Remover" />
-                                    </button>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>Carne bovina</td>
-                                <td>2 kg</td>
-                                <td>
-                                    <button>
-                                        <img src={remover} alt="Remover" />
-                                    </button>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>Creme Dental</td>
-                                <td>1 bisnaga</td>
-                                <td>
-                                    <button>
-                                        <img src={remover} alt="Remover" />
-                                    </button>
-                                </td>
-                            </tr>
                         </tbody>
                     </table>
                 </div>
